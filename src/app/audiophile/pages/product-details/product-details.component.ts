d3721de49d4c744d5d9939952f5d95d88c01ed61
KarimBourass <bourasskarim9@gmail.com>
2021-09-16 22:36:13 +0100

    Add logic to product-details C

--- src/app/audiophile/pages/product-details/product-details.component.ts
+++ src/app/audiophile/pages/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../category/services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: any;
+  productQte = 0
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit(): void {
+    let res = []
+    this.route.parent?.paramMap.subscribe(
+      (data: any) => {
+        res = this.productService.getProductBySlug(data?.params.id);
+        this.product = res[0]
+        console.log('pr', this.product);
+
+      }
+    )
+  }
+
+  minusQte() {
+    if (this.productQte != 0) {
+      this.productQte = this.productQte - 1;
+    }
+  }
+
+  addQte() {
+    this.productQte = this.productQte + 1;
+  }
+
+  addToCart(product: any){
+    console.log('product',product);
+
   }
 
 }
