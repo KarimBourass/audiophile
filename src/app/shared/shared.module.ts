53f90f8138bb198ebd379d1d519bfbeb6fef861f
KarimBourass <bourasskarim9@gmail.com>
2021-09-18 17:22:15 +0100

    Add cart C

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BeforeFooterComponent } from './components/before-footer/before-footer.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { CartComponent } from './components/cart/cart.component';
 
 
 
 @NgModule({
   declarations: [
     BeforeFooterComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    CartComponent
   ],
   imports: [
     CommonModule,
