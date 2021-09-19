119fea074af91fe451a68318d4cc58b2d22e5409
KarimBourass <bourasskarim9@gmail.com>
2021-09-19 12:35:28 +0100

    Adding cart C

--- src/app/shared/components/cart/cart.component.ts
+++ src/app/shared/components/cart/cart.component.ts
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  cart: any[] = [];
+  total: number = 0;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.cart = JSON.parse(localStorage.getItem("products") || "[]");
+    this.calculateTotal();
   }
+
+
+  minusQte(product: any) {
+    this.cart.filter(pro => pro.id == product.id && pro.qte > 0)
+      .map(product => {
+        product.qte = product.qte - 1;
+        localStorage.setItem('products', JSON.stringify(this.cart));
+        this.changeQte("minus", product.price);
+      });
+  }
+
+  addQte(product: any) {
+    this.cart.filter(pro => pro.id == product.id)
+      .map(product => {
+        product.qte = product.qte + 1;
+        localStorage.setItem('products', JSON.stringify(this.cart));
+        this.changeQte("add", product.price);
+      });
+
+  }
+
+  changeQte(operation: string, price: number) {
+    if (operation == "add") this.total = this.total + price
+    else this.total = this.total - price
+  }
+
+
+  calculateTotal() {
+    this.cart.forEach((product: any) => {
+      this.total = this.total + product.price * product.qte;
+    });
+    localStorage.setItem('products', JSON.stringify(this.cart))
+
+  }
+
+  removeAll() {
+    localStorage.setItem('products', JSON.stringify([]));
+    this.cart = [];
+    this.total = 0;
+  }
+
+
 
 }
