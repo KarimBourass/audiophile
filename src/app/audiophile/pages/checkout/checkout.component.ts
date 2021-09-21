5d81d7eb1b18a53a7ecaf1277d0bf5893beb9b2a
KarimBourass <bourasskarim9@gmail.com>
2021-09-21 21:18:04 +0100

    Adding checkout C

---
+++ src/app/audiophile/pages/checkout/checkout.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.scss']
+})
+export class CheckoutComponent implements OnInit {
+
+  cart: any[] = [];
+  total: number = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.cart = JSON.parse(localStorage.getItem("products") || "[]");
+    this.calculateTotal();
+  }
+
+  calculateTotal() {
+    this.cart.forEach((product: any) => {
+      this.total = this.total + product.price * product.qte;
+    });
+    localStorage.setItem('products', JSON.stringify(this.cart))
+
+  }
+}
