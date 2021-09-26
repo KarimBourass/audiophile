4acb9e98731be51122ef136a142db88511b51d55
KarimBourass <bourasskarim9@gmail.com>
2021-09-26 18:17:15 +0100

    Adding order dialog Service

--- src/app/shared/components/services/modal.service.ts
+++ src/app/shared/components/services/modal.service.ts
@@ -1,3 +1,4 @@
+import { OrderDialogComponent } from './../../../audiophile/pages/checkout/components/order-dialog/order-dialog.component';
 import { CartComponent } from './../cart/cart.component';
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
@@ -17,6 +18,13 @@ export class ModalService {
         right: '30px'
       }
     });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openFinalOrderDialog(grandTotal: number) {
+    const dialogRef = this.dialog.open(OrderDialogComponent);
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
