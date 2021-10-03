558775fe1b9cd56d8f8784c888e19288be6508e2
KarinBourass <bourasskarim9@gmail.com>
2021-10-03 19:01:37 +0100

    Add mobile menu

--- src/app/audiophile/layout/header/header.component.ts
+++ src/app/audiophile/layout/header/header.component.ts
@@ -14,8 +14,25 @@ export class HeaderComponent implements OnInit {
 
   showNav = false;
   showBurgerMenu = false;
+  mobielMenu = false;
 
-  constructor(public breakpointObserver: BreakpointObserver,private modalService:ModalService) { }
+  products = [
+    {
+      'imgPath': "../../../../../../assets/shared/desktop/image-category-thumbnail-headphones.png",
+      'name': 'headphones',
+    },
+    {
+      'imgPath': "../../../../../../assets/shared/desktop/image-category-thumbnail-speakers.png",
+      'name': 'speakers',
+
+    },
+    {
+      'imgPath': "../../../../../../assets/shared/desktop/image-category-thumbnail-earphones.png",
+      'name': 'earphones'
+    },
+  ]
+
+  constructor(public breakpointObserver: BreakpointObserver, private modalService: ModalService) { }
 
   ngOnInit(): void {
     this.breakpointObserver
@@ -25,20 +42,22 @@ export class HeaderComponent implements OnInit {
           console.log('Viewport width is 768px or greater!');
           this.showNav = true
           this.showBurgerMenu = false
+          this.mobielMenu = false;
         } else {
           this.showNav = false
           this.showBurgerMenu = true
           console.log('Viewport width is less than 768px!');
         }
       });
+    this.displayNav()
   }
 
-  displayNav(){
-    console.log('dir khdemtek');
-    this.showNav = !this.showNav;
+  displayNav() {
+    this.mobielMenu = !this.mobielMenu;
+    console.log('mobielMenu', this.mobielMenu);
   }
 
-  openCart(){
+  openCart() {
     this.modalService.openCartDialog();
   }
 
