342671e8036da7e95166132b00cedf6337fa01be
KarinBourass <bourasskarim9@gmail.com>
2021-10-09 23:14:37 +0100

    Addng seo service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,38 @@ const routes: Routes = [
   {
     path: 'category/:id',
     loadChildren: () => import('./audiophile/pages/category/category.module').then(m => m.CategoryModule),
+    data: {
+      title: 'Category',
+      description: 'Audiophile category page'
+    }
   },
   {
     path: 'product/:id',
     loadChildren: () => import('./audiophile/pages/product-details/product-details.module').then(m => m.ProductDetailsModule),
-  },
-  {
-    path: '',
-    loadChildren: () => import('./audiophile/pages/home/home.module').then(m => m.HomeModule),
+    data: {
+      title: 'Product',
+      description: 'Audiophile Product page'
+    }
   },
   {
     path: 'checkout',
     loadChildren: () => import('./audiophile/pages/checkout/checkout.module').then(m => m.CheckoutModule),
+    data: {
+      title: 'checkout',
+      description: 'Audiophile checkout page'
+
+    }
+  },
+  {
+    path: '',
+    loadChildren: () => import('./audiophile/pages/home/home.module').then(m => m.HomeModule),
+    data: {
+      title: '',
+      description: 'Audiophile Home page'
+    }
   },
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '**', redirectTo: '/', pathMatch: 'full' },
-
-
 ];
 
 @NgModule({
